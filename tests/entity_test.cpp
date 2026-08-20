aa481948680b797edbb481c9c51449226da8ec60
olesgedz <gedzoles@gmail.com>
2026-08-20 03:54:59 +0300

    cmake

---
+++ tests/entity_test.cpp
@@ -0,0 +1,11 @@
+#include <aegis/aegis.hpp>
+
+#include <gtest/gtest.h>
+#include <type_traits>
+
+TEST(FixedPointTest, DeltaTimeIsPositive)
+{
+    ae::fixed_t dt = ae::fixed_t(1) / 60;
+
+    EXPECT_GT(dt, ae::fixed_t(0));
+}
